fec0c586ed75240bfdb126c3e39d1bd1b320f3e8
Sourav K <souravsourav5919@gmail.com>
2026-06-11 17:25:43 +0530

    Register models in admin site

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView,RetrieveUpdateAPIView
+from rest_framework.permissions import AllowAny,IsAuthenticated
+
+from .serializers import RegisterSerializer,ProfileSerializer
 
 # Create your views here.
+
+class RegisterView(CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+class ProfileView(RetrieveUpdateAPIView):
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
