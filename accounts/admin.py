c97b0c4ea008f93e63357e1ef5621ef45b00e0fc
Sourav K <souravsourav5919@gmail.com>
2026-06-11 18:55:17 +0530

    Customize User model representation in admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+from .models import User
 
 # Register your models here.
 
-User = get_user_model()
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
 
-admin.site.register(User)
+    list_display = (
+        'username',
+        'email',
+        'role',
+        'is_staff',
+    )
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            'LMS Info',
+            {
+                'fields': (
+                    'role',
+                    'bio',
+                )
+            }
+        ),
+    )
