bb56ffa1c14aaccaf2e3936df63105df7186b227
Sourav K <souravsourav5919@gmail.com>
2026-06-13 20:09:19 +0530

    Add ai chat routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,167 +1,90 @@
 import {
-
-    BrowserRouter,
-
-    Routes,
-
-    Route,
-
-    Navigate
-
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate
 } from "react-router-dom";
 
-
 import { AuthProvider } from "./context/AuthContext";
-
 import ProtectedRoute from "./components/ProtectedRoute";
 
-
 import LoginPage from "./pages/LoginPage";
-
 import RegisterPage from "./pages/RegisterPage";
-
 import DashboardPage from "./pages/DashboardPage";
-
 import CourseListPage from "./pages/CourseListPage";
-
 import CourseDetailPage from "./pages/CourseDetailPage";
-
 import LessonPage from "./pages/LessonPage";
-
 import QuizPage from "./pages/QuizPage";
-
-
+import AITutorPage from "./pages/AITutorPage";
 
 function App() {
-
-    return (
-
-        <AuthProvider>
-
-            <BrowserRouter>
-
-                <Routes>
-
-                    <Route
-
-                        path="/"
-
-                        element={<Navigate to="/login" />}
-
-                    />
-
-
-                    <Route
-
-                        path="/login"
-
-                        element={<LoginPage />}
-
-                    />
-
-
-                    <Route
-
-                        path="/register"
-
-                        element={<RegisterPage />}
-
-                    />
-
-
-                    <Route
-
-                        path="/dashboard"
-
-                        element={
-
-                            <ProtectedRoute>
-
-                                <DashboardPage />
-
-                            </ProtectedRoute>
-
-                        }
-
-                    />
-
-
-                    <Route
-
-                        path="/courses"
-
-                        element={
-
-                            <ProtectedRoute>
-
-                                <CourseListPage />
-
-                            </ProtectedRoute>
-
-                        }
-
-                    />
-
-
-                    <Route
-
-                        path="/courses/:id"
-
-                        element={
-
-                            <ProtectedRoute>
-
-                                <CourseDetailPage />
-
-                            </ProtectedRoute>
-
-                        }
-
-                    />
-
-
-                    <Route
-
-                        path="/lessons/:id"
-
-                        element={
-
-                            <ProtectedRoute>
-
-                                <LessonPage />
-
-                            </ProtectedRoute>
-
-                        }
-
-                    />
-
-
-                    <Route
-
-                        path="/quiz/:id"
-
-                        element={
-
-                            <ProtectedRoute>
-
-                                <QuizPage />
-
-                            </ProtectedRoute>
-
-                        }
-
-                    />
-
-                </Routes>
-
-            </BrowserRouter>
-
-        </AuthProvider>
-
-    );
-
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+
+          <Route path="/login" element={<LoginPage />} />
+
+          <Route path="/register" element={<RegisterPage />} />
+
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <DashboardPage />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/courses"
+            element={
+              <ProtectedRoute>
+                <CourseListPage />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/courses/:id"
+            element={
+              <ProtectedRoute>
+                <CourseDetailPage />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/lessons/:id"
+            element={
+              <ProtectedRoute>
+                <LessonPage />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/quiz/:id"
+            element={
+              <ProtectedRoute>
+                <QuizPage />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/lessons/:id/chat"
+            element={
+              <ProtectedRoute>
+                <AITutorPage />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
-
 
 export default App;
