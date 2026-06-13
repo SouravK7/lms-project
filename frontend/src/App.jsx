e9848155c64e7c3a74fb6e7d0920fbd736458b0f
Sourav K <souravsourav5919@gmail.com>
2026-06-13 11:37:33 +0530

    Setup AuthContext, ProtectedRoute, and Axios API client

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,167 @@
-import { useState } from 'react'
+import {
+
+    BrowserRouter,
+
+    Routes,
+
+    Route,
+
+    Navigate
+
+} from "react-router-dom";
+
+
+import { AuthProvider } from "./context/AuthContext";
+
+import ProtectedRoute from "./components/ProtectedRoute";
+
+
+import LoginPage from "./pages/LoginPage";
+
+import RegisterPage from "./pages/RegisterPage";
+
+import DashboardPage from "./pages/DashboardPage";
+
+import CourseListPage from "./pages/CourseListPage";
+
+import CourseDetailPage from "./pages/CourseDetailPage";
+
+import LessonPage from "./pages/LessonPage";
+
+import QuizPage from "./pages/QuizPage";
+
+
 
 function App() {
-  return (
-    <h1>LMS Frontend</h1>
-  )
+
+    return (
+
+        <AuthProvider>
+
+            <BrowserRouter>
+
+                <Routes>
+
+                    <Route
+
+                        path="/"
+
+                        element={<Navigate to="/login" />}
+
+                    />
+
+
+                    <Route
+
+                        path="/login"
+
+                        element={<LoginPage />}
+
+                    />
+
+
+                    <Route
+
+                        path="/register"
+
+                        element={<RegisterPage />}
+
+                    />
+
+
+                    <Route
+
+                        path="/dashboard"
+
+                        element={
+
+                            <ProtectedRoute>
+
+                                <DashboardPage />
+
+                            </ProtectedRoute>
+
+                        }
+
+                    />
+
+
+                    <Route
+
+                        path="/courses"
+
+                        element={
+
+                            <ProtectedRoute>
+
+                                <CourseListPage />
+
+                            </ProtectedRoute>
+
+                        }
+
+                    />
+
+
+                    <Route
+
+                        path="/courses/:id"
+
+                        element={
+
+                            <ProtectedRoute>
+
+                                <CourseDetailPage />
+
+                            </ProtectedRoute>
+
+                        }
+
+                    />
+
+
+                    <Route
+
+                        path="/lessons/:id"
+
+                        element={
+
+                            <ProtectedRoute>
+
+                                <LessonPage />
+
+                            </ProtectedRoute>
+
+                        }
+
+                    />
+
+
+                    <Route
+
+                        path="/quiz/:id"
+
+                        element={
+
+                            <ProtectedRoute>
+
+                                <QuizPage />
+
+                            </ProtectedRoute>
+
+                        }
+
+                    />
+
+                </Routes>
+
+            </BrowserRouter>
+
+        </AuthProvider>
+
+    );
+
 }
 
-export default App
+
+export default App;
