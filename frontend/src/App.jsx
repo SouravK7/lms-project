3957831b2da8a668c60d6170d47767bbfb59f4cb
Sourav K <souravsourav5919@gmail.com>
2026-06-14 13:33:54 +0530

    Edit App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,99 +1,334 @@
 import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate
-} from "react-router-dom";
 
-import { AuthProvider } from "./context/AuthContext";
+  BrowserRouter,
+
+  Routes,
+
+  Route,
+
+  Navigate
+
+}
+
+from "react-router-dom";
+
+
+import {
+
+  AuthProvider
+
+}
+
+from "./context/AuthContext";
+
+
 import ProtectedRoute from "./components/ProtectedRoute";
 
+
 import LoginPage from "./pages/LoginPage";
+
 import RegisterPage from "./pages/RegisterPage";
+
 import DashboardPage from "./pages/DashboardPage";
+
 import CourseListPage from "./pages/CourseListPage";
+
 import CourseDetailPage from "./pages/CourseDetailPage";
+
 import LessonPage from "./pages/LessonPage";
+
 import QuizPage from "./pages/QuizPage";
+
 import AITutorPage from "./pages/AITutorPage";
+
 import ProfilePage from "./pages/ProfilePage";
 
+import CertificatePage from "./pages/CertificatePage";
+
+import InstructorDashboard from "./pages/InstructorDashboard";
+
+import CreateCoursePage from "./pages/CreateCoursePage";
+
+import EditCoursePage from "./pages/EditCoursePage";
+
+import EditLessonPage from "./pages/EditLessonPage";
+
+import EditQuizPage from "./pages/EditQuizPage";
+
+
+
 function App() {
+
   return (
+
     <AuthProvider>
+
       <BrowserRouter>
+
+
         <Routes>
-          <Route path="/" element={<Navigate to="/login" />} />
 
-          <Route path="/login" element={<LoginPage />} />
-
-          <Route path="/register" element={<RegisterPage />} />
 
           <Route
+
+            path="/"
+
+            element={
+
+              <Navigate to="/login" />
+
+            }
+
+          />
+
+
+          <Route
+
+            path="/login"
+
+            element={<LoginPage/>}
+
+          />
+
+
+          <Route
+
+            path="/register"
+
+            element={<RegisterPage/>}
+
+          />
+
+
+          <Route
+
             path="/dashboard"
+
             element={
+
               <ProtectedRoute>
-                <DashboardPage />
+
+                <DashboardPage/>
+
               </ProtectedRoute>
+
             }
+
           />
 
+
           <Route
+
             path="/courses"
+
             element={
+
               <ProtectedRoute>
-                <CourseListPage />
+
+                <CourseListPage/>
+
               </ProtectedRoute>
+
             }
+
           />
 
+
           <Route
+
             path="/courses/:id"
+
             element={
+
               <ProtectedRoute>
-                <CourseDetailPage />
+
+                <CourseDetailPage/>
+
               </ProtectedRoute>
+
             }
+
           />
 
+
           <Route
+
+            path="/certificate/:id"
+
+            element={
+
+              <ProtectedRoute>
+
+                <CertificatePage/>
+
+              </ProtectedRoute>
+
+            }
+
+          />
+
+
+          <Route
+
             path="/lessons/:id"
+
             element={
+
               <ProtectedRoute>
-                <LessonPage />
+
+                <LessonPage/>
+
               </ProtectedRoute>
+
             }
+
           />
 
+
           <Route
+
             path="/quiz/:id"
+
             element={
+
               <ProtectedRoute>
-                <QuizPage />
+
+                <QuizPage/>
+
               </ProtectedRoute>
+
             }
+
+          />
+
+
+          <Route
+
+            path="/lessons/:id/chat"
+
+            element={
+
+              <ProtectedRoute>
+
+                <AITutorPage/>
+
+              </ProtectedRoute>
+
+            }
+
+          />
+
+
+          <Route
+
+            path="/profile"
+
+            element={
+
+              <ProtectedRoute>
+
+                <ProfilePage/>
+
+              </ProtectedRoute>
+
+            }
+
           />
 
           <Route
-            path="/lessons/:id/chat"
+
+            path="/teacher"
+
             element={
+
               <ProtectedRoute>
-                <AITutorPage />
+
+                <InstructorDashboard/>
+
               </ProtectedRoute>
+
             }
+
           />
+
+
           <Route
-            path="/profile"
+
+            path="/teacher/course/create"
+
             element={
+
               <ProtectedRoute>
-                <ProfilePage/>
+
+                <CreateCoursePage/>
+
               </ProtectedRoute>
+
             }
+
           />
+
+
+          <Route
+
+            path="/teacher/course/:id/edit"
+
+            element={
+
+              <ProtectedRoute>
+
+                <EditCoursePage/>
+
+              </ProtectedRoute>
+
+            }
+
+          />
+
+
+          <Route
+
+            path="/teacher/course/:id/lessons"
+
+            element={
+
+              <ProtectedRoute>
+
+                <EditLessonPage/>
+
+              </ProtectedRoute>
+
+            }
+
+          />
+
+
+          <Route
+
+            path="/teacher/quiz/:id/edit"
+
+            element={
+
+              <ProtectedRoute>
+
+                <EditQuizPage/>
+
+              </ProtectedRoute>
+
+            }
+
+          />
+
         </Routes>
+
+
       </BrowserRouter>
+
     </AuthProvider>
+
   );
+
 }
+
 
 export default App;
